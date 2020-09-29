3c87915af6186898621d37d4ad1319f26b7d4d24
mtfuji3776 <32.allegro.con.brio.ed.appassionato@gmail.com>
2020-09-29 21:05:35 +0900

    Reactチュートリアル再開

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,127 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//const element = <h1>Hello, world</h1>; // DOMに見えるのはシンタックスシュガーで実際はオブジェクト
+//ReactDOM.render(element, document.getElementById('root'));
 
-function Square(props){
-/* Componentのconstructorは、state管理のためだけに存在しているようだ。 */
-/* Stateフリーになったコンポーネントを「制御されたコンポーネント」と呼ぶらしい
-    制御されたコンポーネントはクラスよりも関数で定義する方が簡潔に済むらしい */
-
-    
-        return(
-            <button 
-                className = "square" 
-                onClick= {props.onClick}
-            >
-                {props.value}
-            </button>
-        );
-    }
-
-
-
-class Board extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        if(calculateWinner(squares) || squares[i]){
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext
-        });
-    }
-
-    renderSquare(i) {
-        return (<Square 
-                    value={this.state.squares[i]}
-                    onClick={() => this.handleClick(i)}
-        />);
-    }
-    
-    render() {
-        /*const status = 'Next player: ' + (this.state.xIsNext ? 'X':'O');*/
-
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if(winner){
-            status = 'Winner: ' + winner;
-        }else{
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board–row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
+function tick(){
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
 }
 
-
-class Game extends React.Component {
-    render() {
-        return(
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
-function calculateWinner(squares){
-    const lines = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
-    ];
-    for (let i = 0; i < lines.length; i++){
-        const [a,b,c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-            return squares[a];
-        }
-    }
-    return null;
-}
-
-//=================================================
-
-ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-);
+setInterval(tick,1000);
